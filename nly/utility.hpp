bccd13890d536f993865afe4b7e34ee823026f74
szn <80252484@qq.com>
2025-03-16 11:24:11 +0800

    添加了 release_guard

---
+++ nly/utility.hpp
@@ -0,0 +1,31 @@
+﻿#ifndef NLY_UTILITY
+#define NLY_UTILITY
+#include <functional>
+
+namespace nly
+{
+
+// 功能: 保证在对象析构时, 调用一次传入的可执行对象
+class release_guard
+{
+public:
+  release_guard(std::function<void()> fun)
+    : m_fun(fun)
+  {
+  }
+
+  ~release_guard()
+  {
+    if (m_fun)
+    {
+      m_fun();
+    }
+  }
+
+private:
+  std::function<void()> m_fun;
+};
+
+} // namespace nly
+
+#endif
---
+++ tests/source/utility_test.cpp
@@ -0,0 +1,25 @@
+﻿#include "nly/utility.hpp"
+#include "gtest/gtest.h"
+
+TEST(Utility, ReleaseGuard)
+{
+  int value = 0;
+
+  {
+    nly::release_guard guard([&value]() { ++value; });
+  }
+  EXPECT_EQ(value, 1);
+
+  {
+    try
+    {
+      nly::release_guard guard([&value]() { ++value; });
+
+      throw(1);
+    }
+    catch (...)
+    {
+    }
+  }
+  EXPECT_EQ(value, 2);
+}
