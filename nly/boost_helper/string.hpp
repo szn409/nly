7cec4fa4ec61a677065dd6270fa33e093e06c61a
szn <80252484@qq.com>
2025-03-22 22:21:36 +0800

    添加了字符串算法: split

---
+++ nly/boost_helper/string.hpp
@@ -0,0 +1,44 @@
+﻿#ifndef NLY_BOOST_HELPER_STRING
+#define NLY_BOOST_HELPER_STRING
+
+#include "boost/algorithm/string.hpp"
+
+namespace nly
+{
+class string_algo
+{
+public:
+  template<typename t_input, typename t_output, typename t_predicate>
+  static void split_if(
+    t_input&&   input,
+    t_output&   output,
+    t_predicate pred,
+    bool        compress_adjacent_token = false)
+  {
+    boost::split(
+      output,
+      input,
+      pred,
+      compress_adjacent_token ? boost::algorithm::token_compress_on
+                              : boost::algorithm::token_compress_off);
+  }
+
+  template<typename t_input, typename t_output, typename t_input_element>
+  static void split(
+    t_input&&               input,
+    t_output&               output,
+    const t_input_element&& separator,
+    bool                    compress_adjacent_token = false)
+
+  {
+    split_if(
+      input,
+      output,
+      [&separator](const t_input_element& value) { return value == separator; },
+      compress_adjacent_token);
+  }
+};
+
+} // namespace nly
+
+#endif
---
+++ tests/source/boost_helper/string_test.cpp
@@ -0,0 +1,54 @@
+﻿#include "gtest/gtest.h"
+#include "nly/boost_helper/string.hpp"
+#include <string>
+#include <vector>
+
+using std::string;
+using std::u16string;
+using std::u32string;
+using std::vector;
+using std::wstring;
+
+TEST(String, Split)
+{
+  auto fun = [](const char value) { return value == ';'; };
+
+  vector<string> vec;
+  nly::string_algo::split("12;34;56", vec, ';');
+  EXPECT_TRUE(vec == (vector<string>{ "12", "34", "56" }));
+  nly::string_algo::split_if("12;34;56", vec, fun);
+  EXPECT_TRUE(vec == (vector<string>{ "12", "34", "56" }));
+
+  nly::string_algo::split("12;34;56;", vec, ';');
+  EXPECT_TRUE(vec == (vector<string>{ "12", "34", "56", "" }));
+  nly::string_algo::split_if("12;34;56;", vec, fun);
+  EXPECT_TRUE(vec == (vector<string>{ "12", "34", "56", "" }));
+
+  nly::string_algo::split("12;34;;56", vec, ';');
+  EXPECT_TRUE(vec == (vector<string>{ "12", "34", "", "56" }));
+  nly::string_algo::split_if("12;34;;56", vec, fun);
+  EXPECT_TRUE(vec == (vector<string>{ "12", "34", "", "56" }));
+
+  nly::string_algo::split("12;34;;;;56", vec, ';', true);
+  EXPECT_TRUE(vec == (vector<string>{ "12", "34", "56" }));
+  nly::string_algo::split_if("12;34;;;;56", vec, fun, true);
+  EXPECT_TRUE(vec == (vector<string>{ "12", "34", "56" }));
+
+  vector<wstring> out_w;
+  nly::string_algo::split(L"12;34;56", out_w, L';');
+  EXPECT_TRUE(out_w == (vector<wstring>{ L"12", L"34", L"56" }));
+  nly::string_algo::split_if(L"12;34;56", out_w, [](auto v) { return v == L';'; });
+  EXPECT_TRUE(out_w == (vector<wstring>{ L"12", L"34", L"56" }));
+
+  vector<u16string> out_16;
+  nly::string_algo::split(u"12;34;56", out_16, u';');
+  EXPECT_TRUE(out_16 == (vector<u16string>{ u"12", u"34", u"56" }));
+  nly::string_algo::split_if(u"12;34;56", out_16, [](auto v) { return v == u';'; });
+  EXPECT_TRUE(out_16 == (vector<u16string>{ u"12", u"34", u"56" }));
+
+  vector<u32string> out_32;
+  nly::string_algo::split(U"12;34;56", out_32, U';');
+  EXPECT_TRUE(out_32 == (vector<u32string>{ U"12", U"34", U"56" }));
+  nly::string_algo::split_if(U"12;34;56", out_32, [](auto v) { return v == U';'; });
+  EXPECT_TRUE(out_32 == (vector<u32string>{ U"12", U"34", U"56" }));
+}
