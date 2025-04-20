bc24f96bcd306141f9ea797cf5d534ec27c9d6a2
szn <80252484@qq.com>
2025-04-20 22:01:59 +0800

    添加了 serialization 及其测试用例

---
+++ nly/boost_helper/serialization.hpp
@@ -0,0 +1,80 @@
+﻿#ifndef NLY_BOOST_HELPER_SERIALIZATION
+#define NLY_BOOST_HELPER_SERIALIZATION
+#include "boost/archive/binary_iarchive.hpp"
+#include "boost/archive/binary_oarchive.hpp"
+#include "boost/archive/text_iarchive.hpp"
+#include "boost/archive/text_oarchive.hpp"
+#include <string>
+#include <sstream>
+#include <optional>
+
+namespace nly
+{
+
+class serialization
+{
+public:
+  template<typename T>
+  static void to_data(const T& t, std::string& str, bool use_binary = true)
+  {
+    std::ostringstream oss;
+
+    if (use_binary)
+    {
+      boost::archive::binary_oarchive oa(oss);
+      oa & t;
+    }
+    else
+    {
+      boost::archive::text_oarchive oa(oss);
+      oa & t;
+    }
+
+    str = oss.str();
+  }
+
+  template<typename T>
+  static std::string to_data(const T& t, bool use_binary = true)
+  {
+    std::string str;
+    to_data(t, str, use_binary);
+    return std::move(str);
+  }
+
+  template<typename T>
+  static bool from_data(T& t, const std::string& data, bool use_binary = true)
+  {
+    try
+    {
+      std::istringstream iss(data);
+
+      if (use_binary)
+      {
+        boost::archive::binary_iarchive ia(iss);
+        ia & t;
+      }
+      else
+      {
+        boost::archive::text_iarchive ia(iss);
+        ia & t;
+      }
+    }
+    catch (...)
+    {
+      return false;
+    }
+
+    return true;
+  }
+
+  template<typename T>
+  static std::optional<T> from_data(const std::string& data, bool use_binary = true)
+  {
+    T t;
+    return from_data(t, data, use_binary) ? t : std::optional<T>{};
+  }
+};
+
+} // namespace nly
+
+#endif
---
+++ tests/source/boost_helper/serialization_test.cpp
@@ -0,0 +1,40 @@
+﻿#include "gtest/gtest.h"
+#include "nly/boost_helper/serialization.hpp"
+
+class hello
+{
+  friend class boost::serialization::access;
+
+public:
+  template<class Archive>
+  void serialize(Archive& ar, const unsigned int version)
+  {
+    ar & m_int;
+    ar & m_str;
+  }
+
+public:
+  int         m_int{ 0 };
+  std::string m_str{ "hello" };
+};
+
+TEST(Serialization, binary)
+{
+  hello h;
+  h.m_int = 1024;
+  h.m_str = "szn";
+
+  for (int i = 0; i < 2; ++i)
+  {
+    auto content = nly::serialization::to_data(h, i == 0);
+    EXPECT_TRUE(!content.empty());
+
+    auto h1 = nly::serialization::from_data<hello>(content, i == 0);
+    EXPECT_TRUE(h1.has_value());
+    EXPECT_EQ(h1->m_int, 1024);
+    EXPECT_EQ(h1->m_str, "szn");
+
+    auto h2 = nly::serialization::from_data<hello>("", i == 0);
+    EXPECT_TRUE(!h2.has_value());
+  }
+}
