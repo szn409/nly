b3e38a2cfb88c9e06c594bd76ab1a43da3d6f56d
szn <80252484@qq.com>
2025-12-24 22:45:28 +0800

    完成了 cppitertools 的常用函数引用

--- nly/cppitertools.hpp
+++ nly/cppitertools.hpp
@@ -1,6 +1,7 @@
 ﻿#ifndef NLY_CPPITERTOOLS
 #define NLY_CPPITERTOOLS
 #include "cppitertools/itertools.hpp"
+#include "cppitertools/zip_longest.hpp"
 
 namespace nly
 {
@@ -8,7 +9,7 @@ namespace nly
 /*
 1. cppitertools 受 python 启发, 实现了很多其内置函数
 
-2. 迭代过程不涉及底层元素的 复制 或 移动(参考测试用例中的 Zip)
+2. 迭代过程不涉及底层元素的 复制 或 移动
 
 3. 典型用法一(注意范围 for 中的右值引用)
   std::vector<int>         v0{ 1, 2, 3, 4 };
@@ -29,13 +30,49 @@ namespace nly
     sum += v0 * v1;
   }
   EXPECT_TRUE(sum == 2 * 4 + 3 * 5);
+
+5. 有些函数支持嵌套(不深究)
+  std::vector<std::pair<int, int>> result;
+  for (auto [a, b] : nly::zip(nly::range(5), nly::count(20)))
+  {
+    result.emplace_back(a, b);
+  }
+
+  EXPECT_TRUE(
+    result == (std::vector<std::pair<int, int>>{
+                { 0, 20 },
+                { 1, 21 },
+                { 2, 22 },
+                { 3, 23 },
+                { 4, 24 },
+              }));
+
+6. 本文件仅包含了 cppitertools 中最常用的部分函数
 */
 
 using iter::zip;
 
+/*
+1. 类似 zip, 区别在于该函数终止于最长序列
+2. 迭代时的类型为 boost::optional<T&>
+3. 注意: 
+  std::optional<int&> 不合法
+  boost::optional<int&> 合法
+*/
+using iter::zip_longest;
+
 // 注意: 若迭代步长为 0, 将产生空范围(python 会抛出异常)
 // 该函数还支持浮点数类型
 using iter::range;
+
+/*
+等价于 range 没有 stop point
+形式包括
+  count();  // 从 0 开始递增
+  count(i); // 从 i 开始递增
+  count(i, step); // 从 i 开始, 且间隔为 step, step 可以为负
+*/
+using iter::count;
 
 using iter::enumerate;
 
@@ -51,7 +88,15 @@ using iter::filter;
 using iter::filterfalse;
 
 /*
-1. 从可迭代对象中返回元素, 直到遇到第一个不满足谓语的元素, 形式为
+1. 形式为
+  slice(container, start, stop, step = 1)
+2. 返回的范围: [start, stop)
+3. 不同于 python, 目前引用的版本, 不支持 stp 为负数
+*/
+using iter::slice;
+
+/*
+1. 从可迭代对象中返回元素, 直到遇到第一个不满足谓语的元素(不含), 形式为
   takewhile(pred, container);
 2. 若不传递谓语, 则直接测试元素自身是否为 true, 形式为
   takewhile(container);
@@ -68,6 +113,16 @@ using iter::dropwhile;
 
 // 生成可迭代对象的所有值, 并无限循环
 using iter::cycle;
+
+/*
+1. 重复生成同一个元素
+2. 若传入左值则绑定引用, 若传递右值, 则移动引用
+3. 形式一
+  repeat(value); // 将无限生成 value
+4. 形式二
+  repeat(value, count); // 将生成 count 次 value
+*/
+using iter::repeat;
 
 } // namespace nly
 
--- tests/source/cppitertools_test.cpp
+++ tests/source/cppitertools_test.cpp
@@ -89,6 +89,58 @@ TEST(Iter, Zip)
   }
 }
 
+TEST(Iter, ZipLongest)
+{
+  {
+    std::vector<int>         v0{ 1, 2, 3, 4 };
+    std::set<std::string>    v1{ "hello", "world" };
+    std::vector<std::string> result;
+
+    for (auto&& [tmp0, tmp1] : nly::zip_longest(v0, v1))
+    {
+      result.emplace_back(
+        fmt::format(
+          "{}, {}",
+          tmp0.has_value() ? tmp0.value() : 0,
+          tmp1.has_value() ? tmp1.value() : "nothing"));
+    }
+
+    EXPECT_TRUE(
+      result == (std::vector<std::string>{ "1, hello", "2, world", "3, nothing", "4, nothing" }));
+  }
+
+  {
+    int count = 0;
+
+    std::vector<hello> v0;
+    std::vector<int>   v1{ 1, 2, 3 };
+
+    v0.emplace_back(count);
+    v0.emplace_back(count);
+    count = 0;
+
+    int sum = 0;
+    for (auto&& [tmp0, tmp1] : nly::zip_longest(v0, v1))
+    {
+      if (tmp0.has_value() && tmp1.has_value())
+      {
+        sum += tmp0->get();
+        sum += tmp1.value();
+      }
+      else if (tmp0.has_value())
+      {
+        sum += tmp0->get();
+      }
+      else
+      {
+        sum += tmp1.value();
+      }
+    }
+
+    EXPECT_TRUE(0 == count && 6 == sum);
+  }
+}
+
 TEST(Iter, Range)
 {
   std::vector<int> vec;
@@ -139,6 +191,69 @@ TEST(Iter, Range)
     sum += v;
   }
   EXPECT_TRUE(nly::math::float_equal(sum, 10.0));
+}
+
+TEST(Iter, Count)
+{
+  {
+    std::vector<int> vec;
+
+    for (auto value : nly::count())
+    {
+      vec.emplace_back(value);
+      if (vec.size() >= 5)
+      {
+        break;
+      }
+    }
+
+    EXPECT_TRUE(vec == (std::vector<int>{ 0, 1, 2, 3, 4 }));
+  }
+
+  {
+    std::vector<int> vec;
+
+    for (auto value : nly::count(2))
+    {
+      vec.emplace_back(value);
+      if (vec.size() >= 5)
+      {
+        break;
+      }
+    }
+
+    EXPECT_TRUE(vec == (std::vector<int>{ 2, 3, 4, 5, 6 }));
+  }
+
+  {
+    std::vector<int> vec;
+
+    for (auto value : nly::count(2, 2))
+    {
+      vec.emplace_back(value);
+      if (vec.size() >= 5)
+      {
+        break;
+      }
+    }
+
+    EXPECT_TRUE(vec == (std::vector<int>{ 2, 4, 6, 8, 10 }));
+  }
+
+  {
+    std::vector<int> vec;
+
+    for (auto value : nly::count(2, -2))
+    {
+      vec.emplace_back(value);
+      if (vec.size() >= 5)
+      {
+        break;
+      }
+    }
+
+    EXPECT_TRUE(vec == (std::vector<int>{ 2, 0, -2, -4, -6 }));
+  }
 }
 
 TEST(Iter, Enumerate)
@@ -278,6 +393,61 @@ TEST(Iter, FilterFalse)
   }
 }
 
+TEST(Iter, Slice)
+{
+  {
+    const std::vector<int> vec{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+    {
+      std::vector<int> result;
+      for (auto value : nly::slice(vec, 0, 4))
+      {
+        result.emplace_back(value);
+      }
+      EXPECT_TRUE(result == (std::vector<int>{ 0, 1, 2, 3 }));
+    }
+
+    {
+      std::vector<int> result;
+      for (auto value : nly::slice(vec, 1, 4))
+      {
+        result.emplace_back(value);
+      }
+      EXPECT_TRUE(result == (std::vector<int>{ 1, 2, 3 }));
+    }
+
+    {
+      std::vector<int> result;
+      for (auto value : nly::slice(vec, 1, 6, 2))
+      {
+        result.emplace_back(value);
+      }
+      EXPECT_TRUE(result == (std::vector<int>{ 1, 3, 5 }));
+    }
+  }
+
+  {
+    int count = 0;
+
+    std::vector<hello> vec;
+    vec.emplace_back(count);
+    vec.emplace_back(count);
+    vec.emplace_back(count);
+    vec.emplace_back(count);
+    vec.emplace_back(count);
+    vec.emplace_back(count);
+
+    count = 0;
+
+    std::vector<int> result;
+    for (auto&& value : nly::slice(vec, 1, 4, 2))
+    {
+      result.emplace_back(value.get());
+    }
+    EXPECT_TRUE(result == (std::vector<int>{ 0, 0 }));
+  }
+}
+
 TEST(Iter, TakeWhile)
 {
   {
@@ -411,4 +581,51 @@ TEST(Iter, Cycle)
 
     EXPECT_TRUE(result == (std::vector<int>{ 0, 0, 0, 0 }));
   }
+}
+
+TEST(Iter, Repeat)
+{
+  {
+    std::vector<int> result;
+
+    for (auto value : nly::repeat(2))
+    {
+      result.emplace_back(value);
+      if (result.size() >= 4)
+      {
+        break;
+      }
+    }
+
+    EXPECT_TRUE(result == (std::vector<int>{ 2, 2, 2, 2 }));
+  }
+
+  {
+    std::vector<std::string> result;
+
+    for (auto&& value : nly::repeat(std::string("hello"), 3))
+    {
+      result.emplace_back(value);
+    }
+
+    EXPECT_TRUE(result == (std::vector<std::string>{ "hello", "hello", "hello" }));
+  }
+}
+
+TEST(Iter, Nest)
+{
+  std::vector<std::pair<int, int>> result;
+  for (auto [a, b] : nly::zip(nly::range(5), nly::count(20)))
+  {
+    result.emplace_back(a, b);
+  }
+
+  EXPECT_TRUE(
+    result == (std::vector<std::pair<int, int>>{
+                { 0, 20 },
+                { 1, 21 },
+                { 2, 22 },
+                { 3, 23 },
+                { 4, 24 },
+              }));
 }
